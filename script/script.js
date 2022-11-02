48b71bce3879ecf16a4c024e8eb4a125ccee16ac
JorgeMaiconFaria <jorgemaicon.faria@gmail.com>
2022-11-02 10:39:17 -0300

    Update script.js

--- script/script.js
+++
@@ -1,8 +0,0 @@
-function calcStr() {
-    var str = document.getElementById('force').value;
-    var strMod = document.getElementById('force-mod').value;
-    document.getElementById('force-total').value = Math.floor((parseInt(str) + parseInt(strMod)));
-}
-
-
-
