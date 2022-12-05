9389a7b2aead99cce8785a93f0549b0f4e5b3ec6
JorgeMaiconFaria <jorgemaicon.faria@gmail.com>
2022-12-05 17:35:35 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,26 +2,31 @@
 function calcStr() {
     var str = document.getElementById('force').value;
     var strMod = document.getElementById('force-mod').value;
+    document.getElementById('force-total').innerHTML = Math.floor((parseInt(str) + parseInt(strMod)));
     return Math.floor((parseInt(str) + parseInt(strMod)));};
 
 function calcAgil() {
     var agil = document.getElementById('agil').value;
     var agilMod = document.getElementById('agil-mod').value;
+    document.getElementById('agil-total').innerHTML = Math.floor((parseInt(agil) + parseInt(agilMod)));
     return Math.floor((parseInt(agil) + parseInt(agilMod)));};
 
 function calcInt() {
     var int = document.getElementById('int').value;
     var intMod = document.getElementById('int-mod').value;
+    document.getElementById('int-total').innerHTML = Math.floor((parseInt(int) + parseInt(intMod)));
     return Math.floor((parseInt(int) + parseInt(intMod)));};
 
 function calcTH() {
     var tH = document.getElementById('tec.hab').value;
     var tHMod = document.getElementById('tec.hab-mod').value;
+    document.getElementById('tec-hab-total').innerHTML = Math.floor((parseInt(tH) + parseInt(tHMod)));
     return Math.floor((parseInt(tH) + parseInt(tHMod)));};
 
 function calcCool() {
     var cool = document.getElementById('cool').value;
     var coolMod = document.getElementById('cool-mod').value;
+    document.getElementById('cool-total').innerHTML = Math.floor((parseInt(cool) + parseInt(coolMod)));
     return Math.floor((parseInt(cool) + parseInt(coolMod)));};
 
 const spanPlayer = document.getElementById('spanPlayer');
@@ -138,3 +143,4 @@ window.onload = () => {
     spanFac.innerHTML = playerFac;
 
 };
+
